ae2429c53294ecad85489700b7af074e6437a226
mdowns1999 <michael.downs@familysearch.org>
2025-09-25 12:01:57 -0600

    Started Work on Navigation

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -5,27 +5,47 @@ import Image from "../../components/UI/Image/Image";
 import logo from "../../assets/react.svg";
 import Row from "../../components/UI/Row/Row";
 import { Container } from "@mui/material";
-
+import MobileMenu from "../../components/Menu/Menu";
+import useIsMobile from "../../hooks/useIsMobile";
 
 const navListCss = css`
   display: flex;
   gap: 1em;
+  list-style: none;
+  margin: 0;
+  padding: 0;
 `;
 
+const headerLayout = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding:20px;
+`;
+
+
 export default function Header() {
+  const isMobile = useIsMobile();
+
   return (
     <header>
-      <Container maxWidth='xl'>
-        <Row alignX="between" alignY="center">
-          <Image to="/" src={logo} alt="Downs Portfolio Logo" />
+      <Container maxWidth="xl">
+        <div css={headerLayout}>
+          <Row alignY="middle">
+            <Image to="/" src={logo} alt="Downs Portfolio Logo" />
 
-          <nav>
-            <ul css={navListCss}>
-              <NavLink to="/">Home</NavLink>
-              <NavLink to="/trip">Plan Trip</NavLink>
-            </ul>
-          </nav>
-        </Row>
+            {!isMobile && (
+              <nav>
+                <ul css={navListCss}>
+                  <NavLink to="/">Home</NavLink>
+                  <NavLink to="/trip">Plan Trip</NavLink>
+                </ul>
+              </nav>
+            )}
+          </Row>
+          {isMobile && <MobileMenu />}
+        </div>
       </Container>
     </header>
   );
